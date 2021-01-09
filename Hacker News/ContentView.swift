dce879d3832dc32b5af020dfa1d6913386135d38
Majd <m3khadra@gmail.com>
2021-01-08 22:31:23 -0500

    first commit

---
+++ Hacker News/ContentView.swift
@@ -0,0 +1,38 @@
+ //
+ //  ContentView.swift
+ //  Hacker News
+ //
+ //  Created by Majd Khadra on 2020-12-04.
+ //
+ 
+ import SwiftUI
+ 
+ struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            List(posts) { post in
+                Text(post.title)
+            }
+            .navigationBarTitle("HACKER NEWS")
+        }
+    }
+ }
+ 
+ struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+ }
+
+ 
+ struct Post: Identifiable {
+    let id: String
+    let title: String
+    
+ }
+
+ let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Bounjour"),
+    Post(id: "3", title: "Hola")
+ ]
